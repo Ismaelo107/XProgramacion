437f55bb568bc3ad48b3621e653c43e878efa0bd
Ismaelo107 <ismaeloeg@iescastelar.com>
2022-02-03 13:20:27 +0100

    He añadido la 2ª PERSONA

--- XProgramacion/src/Actividad1/App.java
+++ XProgramacion/src/Actividad1/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Instroducir datos de la primare persona: ");
         System.out.println("Introducir Id: ");
         int id = sc.nextInt();
         System.out.println("Introducir DNI: ");
@@ -19,8 +19,23 @@ public class App {
         System.out.println("Introducir TELEFONO: ");
         int telefono = sc.nextInt();
 
-        Persona p1 = new Persona(id,dni,nombre,direccion,telefono);
+        Persona p1 = new Persona(id, dni, nombre, direccion, telefono);
+
+        System.out.println("Introducir datos de la segunda persona: ");
+        System.out.println("Introducir Id: ");
+        id = sc.nextInt();
+        System.out.println("Introducir DNI: ");
+        dni = sc.nextInt();
+        System.out.println("Introducir NOMBRE: ");
+        nombre = sc.next();
+        System.out.println("Introducir DIRECCION: ");
+        direccion = sc.next();
+        System.out.println("Introducir TELEFONO: ");
+        telefono = sc.nextInt();
+
+        Persona p2 = new Persona(id, dni, nombre, direccion, telefono);
 
         System.out.println(p1.pressPersona());
+        System.out.println(p2.pressPersona());
     }
 }
